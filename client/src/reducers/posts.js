fede9c79aa100d1e6cff8a8427a03a64abc9b951
Ege Ergül <63500329+egeergul@users.noreply.github.com>
2023-01-22 20:42:59 +0300

    pagination works

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,21 +1,25 @@
 import { CREATE, DELETE, FETCH_ALL, UPDATE, LIKE, FETCH_BY_SEARCH } from "../constants/actionTypes";
 
-export default (posts = [], action)=> {
+export default (state = [], action)=> {
     
     switch (action.type) {
         case FETCH_ALL:
-            return action.payload;
+            return {...state, 
+                posts: action.payload.data,
+                currentPage :action.payload.currentPage,
+                numberOfPages: action.payload.numberOfPages
+            };
         case CREATE:
-            return [...posts, action.payload];
+            return [...state, action.payload];
         case UPDATE:
         case LIKE:
-            return posts.map((post)=> post._id === action.payload._id ? action.payload : post);
+            return state.map((post)=> post._id === action.payload._id ? action.payload : post);
         case DELETE:
-            return posts.filter((post)=> post._id !== action.payload );
+            return state.filter((post)=> post._id !== action.payload );
         case FETCH_BY_SEARCH:
-            return action.payload;
+            return {...state, posts: action.payload.data};
         default:
-            return posts;
+            return state;
             
         
     }
